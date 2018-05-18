c3af5ee862aba0531ce3eaa5478fcc96ba3df188
yasintar <32873398+yasintar@users.noreply.github.com>
2018-05-18 16:39:44 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,428 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Rona Clothing</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Rona Clothing" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/simpleCart.min.js"> </script>
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<!--webfont-->
+<link href='//fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+<!--//webfont-->
+<script src="js/jquery.easydropdown.js"></script>
+<!-- Add fancyBox main JS and CSS files -->
+<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
+<link href="css/magnific-popup.css" rel="stylesheet" type="text/css">
+		<script>
+			$(document).ready(function() {
+				$('.popup-with-zoom-anim').magnificPopup({
+					type: 'inline',
+					fixedContentPos: false,
+					fixedBgPos: true,
+					overflowY: 'auto',
+					closeBtnInside: true,
+					preloader: false,
+					midClick: true,
+					removalDelay: 300,
+					mainClass: 'my-mfp-zoom-in'
+			});
+		});
+		</script>
+</head>
+<body>
+<div class="header">	
+      <div class="container"> 
+         <div class="header-top">
+      		 <div class="logo">
+				<a href="index.php"><h6>Online Shop</h6><h2> RO..NA </h2></a>
+			 </div>
+		   <div class="header_right">
+			 <ul class="social">
+				<li><a href=""> <i class="fb"> </i> </a></li>
+				<li><a href=""><i class="tw"> </i> </a></li>
+				<li><a href=""><i class="utube"> </i> </a></li>
+				<li><a href=""><i class="pin"> </i> </a></li>
+				<li><a href=""><i class="instagram"> </i> </a></li>
+			 </ul>
+		    <div class="lang_list">
+			  <select tabindex="5" class="dropdown">
+				<option value="" class="label" value="">payment</option>
+				<option value="1">BRI</option>
+				<option value="2">BNI</option>
+				<option value="3">BCA</option>
+				<option value="4">Mandiri</option>
+			  </select>
+   			</div>
+			<div class="clearfix"></div>
+          </div>
+          <div class="clearfix"></div>
+		 </div>  
+		 <div class="banner_wrap">
+			<div class="bannertop_box">
+   		 		<ul class="login">
+   		 			<li class="login_text"><a href="login.php">Login</a></li>
+   		 			<li class="wish"><a href="checkout.php">Wish List</a></li>
+   		 			<div class='clearfix'></div>
+   		 		</ul>
+   		 		<div class="cart_bg">
+	   		 	  <ul class="cart">
+	   		 		 <a href="checkout.php">
+					    <h4><i class="cart_icon"> </i><p>Cart: <span class="simpleCart_total"></span> (<span id="simpleCart_quantity" class="simpleCart_quantity"></span> items)</p><div class="clearfix"> </div></h4>
+					 </a>
+				     <h5 class="empty"><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></h5>
+				     <div class="clearfix"> </div>
+                  </ul>
+	   		 	</div>
+			  	<ul class="quick_access">
+   		 			<li class="view_cart"><a href="checkout.php">View Cart</a></li>
+   		 			<li class="check"><a href="checkout.php">Checkout</a></li>
+   		 			<div class='clearfix'></div>
+   		 		</ul>
+   		 		<div class="search">
+	  			   <input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
+				   <input type="submit" value="">
+	  			</div>
+	  			<div class="welcome_box">
+	  				<h3>Welcome to </br>R O N A</h3>
+	  				<p>It is a long established fact that a reader will be distracted by the readable content of a page</p>
+	  			</div>
+   		 	</div>
+   		 	<div class="banner_right">
+   		 		<h1>R O N A <br>c l o t h i n g</h1>
+   		 		<p>we lead the way to be fashionista!</p>
+   		 		<a href="#" class="banner_btn">Buy Now</a>
+   		 	</div>
+   		 	<div class='clearfix'></div>
+	    </div>
+	   </div>
+	</div>
+	<div class="main">
+	  <div class="content_box">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					<div class="menu_box">
+				   	  	<h3 class="menu_head">Menu</h3>
+				   	     <ul class="nav">
+					   	  	<li><a href="about.php">Women</a></li>
+					   	  	<li><a href="about.php">Men</a></li>
+					   	  	<li><a href="about.php">Kids</a></li>
+					   	 </ul>
+			   	    </div>
+			   	    <div class="side_banner">
+					   <div class="banner_img"><img src="images/isopen.jpg" class="img-responsive" alt=""/></div>
+					   <div class="banner_holder">
+						  <h3>Now <br> is <br> Open!</h3>
+					   </div>
+				    </div>
+				    <div class="tags">
+				    	<h4 class="tag_head">Tags Widget</h4>
+				         <ul class="tags_links">
+							<li><a href="#">Casual</a></li>
+							<li><a href="#">Formal</a></li>
+							<li><a href="#">Recommended</a></li>
+							<li><a href="#">Colorful</a></li>
+							<li><a href="#">Modest</a></li>
+							<li><a href="#">Women</a></li>
+							<li><a href="#">Men</a></li>
+							<li><a href="#">Kids</a></li>
+						</ul>
+						
+				    </div>
+				     <div class="tags">
+				      	<h4 class="tag_head">Terms and Conditions</h4>
+				      	 <ul class="article_links">
+				      	 	<li>1. All items are new.</li>
+							<li>2. You can return the item within 14 days of receiving it.</li>
+							<li>3. The item is returned unworn, unwashed and in original condition.</li>
+							<li>4. All original labels and tags are still attached to the item.</li>
+							<li>5. You provide the original RONA Clothing receipt as proof of purchase.</li>
+							<li>6. All prices are shown in Indonesian's Rupiah (Rp).</li>
+							<li>7. Accessing the website means that you agree to the terms and conditions.</li>
+							<li>8. These terms and conditions apply to www.ronaclothing.co.id.</li>
+							<li>9. Our customer service will endeavor to respond to your enquiry within 24 hours.</li>
+							<li>10.Thank you for your cooperation. Happy shopping ^^)/</li>
+							<br>
+							<li>Best Regards,</li>
+							<li>Rona Weasley</li>
+						  </ul>
+				      </div>
+				</div>
+			   <div class="col-md-9">
+				<h3 class="m_1">W O M E N</h3>
+				 <div class="content_grid">
+				   <div class="col_1_of_3 span_1_of_3 simpleCart_shelfItem"> 
+				  	   <a href="single.php">
+						   <div class="inner_content clearfix">
+							<div class="product_image">
+								<img src="images/grpolkablazer.jpg" class="img-responsive" alt=""/>
+								<a href="" class="button item_add item_1"> </a>	
+								 <div class="product_container">
+								   <div class="cart-left">
+									 <p class="title">Grey Polka Blazer</p>
+								   </div>
+								   <span class="amount item_price">Rp140.000</span>
+								   <div class="clearfix"></div>
+							     </div>		
+							  </div>
+		                    </div>
+		                 </a>
+				    </div>
+				    <div class="col_1_of_3 span_1_of_3 simpleCart_shelfItem"> 
+				  	   <a href="single.php">
+						   <div class="inner_content clearfix">
+							<div class="product_image">
+								<img src="images/bblongouter.jpg" class="img-responsive" alt=""/>
+								<a href="" class="button item_add item_1"> </a>		
+								<div class="product_container">
+								   <div class="cart-left">
+									 <p class="title">Black Long Outer</p>
+								   </div>
+								   <span class="amount item_price">Rp180.000</span>
+								   <div class="clearfix"></div>
+							     </div>		
+							</div>
+						</div>
+		                 </a>
+				    </div>
+				    <div class="col_1_of_3 span_1_of_3 simpleCart_shelfItem last_1"> 
+				  	      <a href="single.php">
+						   <div class="inner_content clearfix">
+							<div class="product_image">
+								<img src="images/navycullote.jpg" class="img-responsive" alt=""/>
+								<a href="" class="button item_add item_1"> </a>	
+								<div class="product_container">
+								   <div class="cart-left">
+									 <p class="title">Navy Tie Up Cullote</p>
+								   </div>
+								   <span class="amount item_price">Rp150.000</span>
+								   <div class="clearfix"></div>
+							     </div>		
+							  </div>
+		                    </div>
+		                 </a>
+				   </div>
+				   <div class="clearfix"></div>
+				   <a href="#" class="link1">View all products for women</a>
+			  </div>
+			  <h3 class="m_2">M E N</h3>
+			   <div class="content_grid">
+			   		<div class="col_1_of_3 span_1_of_3 simpleCart_shelfItem"> 
+				  	   <a href="single.php">
+						   <div class="inner_content clearfix">
+							<div class="product_image">
+								<img src="images/roundneck.jpg" class="img-responsive" alt=""/>
+								<a href="" class="button item_add item_1"> </a>		
+							    <div class="product_container">
+								   <div class="cart-left">
+									 <p class="title">Round Neck T-Shirt</p>
+								   </div>
+								   <span class="amount item_price">Rp90.000</span>
+								   <div class="clearfix"></div>
+							     </div>		
+							  </div>
+		                    </div>
+		                 </a>
+				    </div>
+				    <div class="col_1_of_3 span_1_of_3 simpleCart_shelfItem"> 
+				  	     <a href="single.php">
+						   <div class="inner_content clearfix">
+							<div class="product_image">
+								<img src="images/mensporty.jpg" class="img-responsive" alt=""/>
+								<a href="" class="button item_add item_1"> </a>		
+							    <div class="product_container">
+								   <div class="cart-left">
+									 <p class="title">Sporty Jacket</p>
+								   </div>
+								   <span class="amount item_price">Rp230.000</span>
+								   <div class="clearfix"></div>
+							     </div>		
+							  </div>
+		                    </div>
+		                 </a>
+				    </div>
+				    <div class="col_1_of_3 span_1_of_3 simpleCart_shelfItem last_1"> 
+				  	  <a href="single.php">
+						   <div class="inner_content clearfix">
+							<div class="product_image">
+								<img src="images/fitshorts.jpg" class="img-responsive" alt=""/>
+								<a href="" class="button item_add item_1"> </a>	
+								 <div class="product_container">
+								   <div class="cart-left">
+									 <p class="title">Regular Fit Shorts</p>
+								   </div>
+								   <span class="amount item_price">Rp90.000</span>
+								   <div class="clearfix"></div>
+							     </div>		
+							  </div>
+		                    </div>
+		                 </a>
+				    </div>
+				    <div class="clearfix"></div>
+				    <a href="#" class="link1">View all products for men</a>
+			    </div>
+			    <h3 class="m_2">K I D S</h3>
+			   <div class="content_grid">
+			   		<div class="col_1_of_3 span_1_of_3 simpleCart_shelfItem"> 
+				  	   <a href="single.php">
+						   <div class="inner_content clearfix">
+							<div class="product_image">
+								<img src="images/parkayellow.jpg" class="img-responsive" alt=""/>
+								<a href="" class="button item_add item_1"> </a>		
+								<div class="product_container">
+								   <div class="cart-left">
+									 <p class="title">Parka Yellow</p>
+								   </div>
+								   <span class="amount item_price">Rp140.000</span>
+								   <div class="clearfix"></div>
+							     </div>		
+							  </div>
+		                    </div>
+		                 </a>
+				    </div>
+				    <div class="col_1_of_3 span_1_of_3 simpleCart_shelfItem"> 
+				  	   <a href="single.php">
+						   <div class="inner_content clearfix">
+							<div class="product_image">
+								<img src="images/jumpsuit.jpg" class="img-responsive" alt=""/>
+								<a href="" class="button item_add item_1"> </a>	
+								<div class="product_container">
+								   <div class="cart-left">
+									 <p class="title">Shoulder Jumpsuit</p>
+								   </div>
+								   <span class="amount item_price">Rp90.000</span>
+								   <div class="clearfix"></div>
+							     </div>		
+							  </div>
+		                    </div>
+		                 </a>
+				    </div>
+				    <div class="col_1_of_3 span_1_of_3 simpleCart_shelfItem last_1"> 
+				  	    <a href="single.php">
+						   <div class="inner_content clearfix">
+							<div class="product_image">
+								<img src="images/spicyprint.jpg" class="img-responsive" alt=""/>
+								<a href="" class="button item_add item_1"> </a>	
+								<div class="product_container">
+								   <div class="cart-left">
+									 <p class="title">Spicy Print Sweater</p>
+								   </div>
+								   <span class="amount item_price">Rp80.000</span>
+								   <div class="clearfix"></div>
+							     </div>		
+							  </div>
+		                    </div>
+		                 </a>
+				   </div>
+				   <div class="clearfix"></div>
+				   <a href="#" class="link1">View all products for kids</a>
+			    </div>
+			  </div>
+			</div>
+		 </div>
+		</div>
+		<div class="container">
+			<div class="brands">
+				<ul class="brand_icons">
+					<li><img src='images/icon1.png' class="img-responsive" alt=""/></li>
+					<li><img src='images/icon2.png' class="img-responsive" alt=""/></li>
+					<li><img src='images/icon3.png' class="img-responsive" alt=""/></li>
+					<li><img src='images/icon4.png' class="img-responsive" alt=""/></li>
+					<li class="last"><img src='images/icon5.png' class="img-responsive" alt=""/></li>
+				</ul>
+			</div>
+	    </div>
+	    <div class="container">
+			<div class="instagram_top">
+				<div class="instagram text-center">
+					<h3><a href="index.php">More Collections</a></h3>
+				</div>
+				<ul class="instagram_grid">
+				  <li><a class="popup-with-zoom-anim" href="#small-dialog1"><img src="images/sandal.jpg" class="img-responsive" alt=""/></a></li>
+				  <li><a class="popup-with-zoom-anim" href="#small-dialog1"><img src="images/tape.jpg" class="img-responsive" alt=""/></a></li>
+				  <li><a class="popup-with-zoom-anim" href="#small-dialog1"><img src="images/pumps.jpg" class="img-responsive" alt=""/></a></li>
+				  <li><a class="popup-with-zoom-anim" href="#small-dialog1"><img src="images/loaf.jpg" class="img-responsive" alt=""/></a></li>
+				  <li><a class="popup-with-zoom-anim" href="#small-dialog1"><img src="images/sneak.jpg" class="img-responsive" alt=""/></a></li>
+				  <li class="last_instagram"><a class="popup-with-zoom-anim" href="#small-dialog1"><img src="images/wedges.jpg" class="img-responsive" alt=""/></a></li>
+				  <div class="clearfix"></div>
+				  <div id="small-dialog1" class="mfp-hide">
+					<div class="pop_up">
+						<h4>A Sample Photo Stream</h4>
+						<img src="images/i_zoom.jpg" class="img-responsive" alt=""/>
+					</div>
+				  </div>
+				</ul>
+			</div>
+			<ul class="footer_social">
+				<li><a href="https://twitter.com/hmtc_its" target="_blank" rel="nofollow" title="RN TW"> <i class="tw"> </i> </a></li>
+				<li><a href="https://www.facebook.com/HMTCFTIf/" target="_blank" rel="nofollow" title="RN FB"> <i class="fb"> </i> </a></li>			
+				<li><a href="#"><i class="pin"> </i> </a></li>
+				<div class="clearfix"></div>
+			</ul>
+	    </div>
+    </div>
+    <div class="footer">
+		<div class="container">
+			<div class="footer-grid">
+				<h3>Category</h3>
+				<ul class="list1">
+				  <li><a href="index.php">Home</a></li>
+				  <li><a href="about.php">About us</a></li>
+				  <li><a href="about.php">Eshop</a></li>
+				  <li><a href="about.php">Features</a></li>
+				  <li><a href="about.php">New Collections</a></li>
+				  <li><a href="typo.php">Typo</a></li>
+				  <li><a href="contact.php">Contact</a></li>
+				</ul>
+			</div>
+			<div class="footer-grid">
+				<h3>Our Account</h3>
+				<ul class="list1">
+				  <li><a href="login.php">Your Account</a></li>
+				  <li><a href="contact.php">Personal information</a></li>
+				  <li><a href="contact.php">Addresses</a></li>
+				  <li><a href="#">Discount</a></li>
+				  <li><a href="checkout.php">Orders history</a></li>
+				  <li><a href="about.php">Search Terms</a></li>
+				</ul>
+			</div>
+			<div class="footer-grid">
+				<h3>Our Support</h3>
+				<ul class="list1">
+				  <li><a href="contact.php">Site Map</a></li>
+				  <li><a href="about.php">Search Terms</a></li>
+				  <li><a href="about.php">Advanced Search</a></li>
+				  <li><a href="about.php">Mobile</a></li>
+				  <li><a href="contact.php">Contact Us</a></li>
+				  <li><a href="contact.php">Mobile</a></li>
+				  <li><a href="contact.php">Addresses</a></li>
+				</ul>
+			</div>
+			<div class="footer-grid">
+				<h3>Newsletter</h3>
+				<p class="footer_desc">Nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat</p>
+				<div class="search_footer">
+				  <input type="text" class="text" value="Your Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Your Email';}">
+				  <input type="submit" value="Subscribe">
+				</div>
+				<img src="images/payment.png" class="img-responsive" alt=""/>
+			</div>
+			 <div class="clearfix"> </div>
+		</div>
+	</div>
+	<div class="footer_bottom">
+		<div class="container">
+			<div class="copy">
+			   <p>Copyright &copy; 2018 RONA Clothing. All Rights Reserved</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>		
